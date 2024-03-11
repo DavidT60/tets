6f473180c1a3a681de458443a95ae9b07226eea9
DavidT60 <76856429+DavidT60@users.noreply.github.com>
2024-03-11 01:28:18 -0400

    Add files via upload

---
+++ library-3-3-24/__init__.py
@@ -0,0 +1,41 @@
+
+from trytond.pool import Pool
+
+from . import library
+from . import wizard
+
+
+print("Testing library module")
+
+"""
+    register:
+      it will be called by the server during the server start up to register the module. 
+
+      it will create the tables based in the register and the class's __name__ on it
+      .... library_authoer
+
+      it will look in the magic library.Author __name__
+"""
+def register():
+     print('server calling register pool.....')
+     Pool.register(
+          library.Author,
+          library.Genre,
+          library.Book,
+          library.BookEditor,
+          library.EditorGenreRelation, 
+          library.BookComment,
+          library.BookComentRelation,
+          library.BooksGenre,
+          wizard.CreateExemplariesParametres,
+          wizard.CreateViewBook,
+          wizard.fullingCreateExemplariesParametres,
+          module='library',
+          type_='model'
+      )
+     Pool.register(
+          wizard.CreateExemplaries,
+          wizard.CreateBooks,
+          module='library',
+          type_='wizard',
+     )
